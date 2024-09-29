7a7213b1bfdb6442d0c83994fd1dc819872f5e19
Mohamed Badr <eng.ma.badr@gmail.com>
2024-09-29 15:15:58 +0300

    Add project files.

---
+++ SqlBulkTools/Core/EnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Data;
+
+namespace SqlBulkTools.Core
+{
+    internal static class EnumerableExtensions
+    {
+        internal static DataTable ToDataTable<T>(this IEnumerable<T> data)
+        {
+            var properties = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+
+            foreach (var item in data)
+            {
+                var row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+    }
+}
---
+++ SqlBulkTools/SqlBulkTools.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using SqlBulkTools.Core;
+
+namespace SqlBulkTools
+{
+    public class SqlBulkTools
+    {
+        private string _connectionString = null!;
+        private string _tableName = null!;
+        private int _commitBatchSize = 1000;
+
+        public void SetConnectionString(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void SetTableName(string tableName)
+        {
+            _tableName = tableName;
+        }
+
+        public void SetCommitBatchSize(int commitBatchSize)
+        {
+            _commitBatchSize = commitBatchSize;
+        }
+
+        public async Task BulkInsertAsync<T>(IList<T> data, string tableName)
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+            await using var transaction = await connection.BeginTransactionAsync();
+
+            try
+            {
+                var pages = (data.Count / _commitBatchSize) + (data.Count % _commitBatchSize == 0 ? 0 : 1);
+                for (var page = 0; page < pages; page++)
+                {
+                    var dt = data.Skip(page * _commitBatchSize).Take(_commitBatchSize).ToDataTable();
+                    await BulkInsert(dt, connection, transaction);
+                }
+
+                await transaction.CommitAsync();
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync();
+                await connection.CloseAsync();
+
+                throw;
+            }
+        }
+
+        private async Task BulkInsert(DataTable dt, SqlConnection connection, IDbTransaction transaction)
+        {
+            var bulkCopy =
+                new SqlBulkCopy
+                (
+                    connection,
+                    SqlBulkCopyOptions.TableLock |
+                    SqlBulkCopyOptions.FireTriggers,
+                    transaction as SqlTransaction
+                )
+                {
+                    DestinationTableName = _tableName
+                };
+
+
+            await bulkCopy.WriteToServerAsync(dt);
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(_connectionString))
+                throw new InvalidEnumArgumentException("Connection string is not set.");
+
+            if (string.IsNullOrWhiteSpace(_tableName))
+                throw new InvalidEnumArgumentException("Table name is not set.");
+        }
+    }
+}
